d9b1b9465d0cd4b82e721a80151912d6d8d22a9e
Nathaniel Green <ngreen2303@gmail.com>
2018-02-22 22:45:49 +0000

    UG-2: Updated ESLint rules for state mutation and added Vuex User

--- store/index.js
+++ store/index.js
@@ -6,13 +6,18 @@ const store = () => new Vuex.Store({
     user: null,
   },
   getters: {
+    getUser: state => state.user,
   },
   mutations: {
+    updateUser(state, value) {
+      state.user = value;
+    },
   },
   actions: {
     SIGN_IN(context, signInDetails) {
       firebase.auth().signInWithEmailAndPassword(signInDetails.email, signInDetails.password)
-        .then(() => {
+        .then((res) => {
+          context.commit('updateUser', res);
           this.$router.replace('/');
         })
         .catch((err) => {
