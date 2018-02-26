c7e67a31c627f98c0b57fecbdc21d2cacd23e2b1
Nathaniel Green <ngreen2303@gmail.com>
2018-02-26 12:28:11 +0000

    UG-19: Intialised Navbar with basic setup

---
+++ components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__links">
+      <nuxt-link
+        class="navbar__links-link navbar__links-link--one"
+        to="/login"
+      >Login</nuxt-link>
+      <nuxt-link
+        class="navbar__links-link navbar__links-link--two"
+        to="/signup"
+      >Sign Up</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables.scss';
+
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+
+  &__links {
+    display: grid;
+    grid-template-columns: 8fr 1fr 1fr;
+    grid-template-areas:
+      ". one two";
+    width: 100%;
+
+    &-link {
+      align-self: center;
+      color: $color-white;
+      border-bottom: solid 1px;
+      padding: 5px;
+      justify-self: center;
+
+      &--one {
+        grid-area: one;
+      }
+      &--two {
+        grid-area: two;
+      }
+    }
+  }
+}
+</style>
